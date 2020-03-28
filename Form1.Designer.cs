433cfc71c52660165d386ce5bf018a8cde376e1c
zart227 <39973078+zart227@users.noreply.github.com>
2020-03-28 12:56:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace FIRfilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dfPictureBox = new System.Windows.Forms.PictureBox();
+            this.signalButton = new System.Windows.Forms.Button();
+            this.irButton = new System.Windows.Forms.Button();
+            this.dfButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.irPictureBox = new System.Windows.Forms.PictureBox();
+            this.signalPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dfPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.irPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signalPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dfPictureBox
+            // 
+            this.dfPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dfPictureBox.BackColor = System.Drawing.SystemColors.Window;
+            this.dfPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dfPictureBox.Location = new System.Drawing.Point(2, 355);
+            this.dfPictureBox.Name = "dfPictureBox";
+            this.dfPictureBox.Size = new System.Drawing.Size(829, 150);
+            this.dfPictureBox.TabIndex = 2;
+            this.dfPictureBox.TabStop = false;
+            // 
+            // signalButton
+            // 
+            this.signalButton.Enabled = false;
+            this.signalButton.Location = new System.Drawing.Point(2, 543);
+            this.signalButton.Name = "signalButton";
+            this.signalButton.Size = new System.Drawing.Size(79, 36);
+            this.signalButton.TabIndex = 2;
+            this.signalButton.Text = "Исходый сигнал";
+            this.signalButton.UseVisualStyleBackColor = true;
+            this.signalButton.Click += new System.EventHandler(this.signalButton_Click);
+            // 
+            // irButton
+            // 
+            this.irButton.Enabled = false;
+            this.irButton.Location = new System.Drawing.Point(87, 543);
+            this.irButton.Name = "irButton";
+            this.irButton.Size = new System.Drawing.Size(76, 36);
+            this.irButton.TabIndex = 3;
+            this.irButton.Text = "Характеристика h(i)";
+            this.irButton.UseVisualStyleBackColor = true;
+            this.irButton.Click += new System.EventHandler(this.irButton_Click);
+            // 
+            // dfButton
+            // 
+            this.dfButton.Enabled = false;
+            this.dfButton.Location = new System.Drawing.Point(169, 543);
+            this.dfButton.Name = "dfButton";
+            this.dfButton.Size = new System.Drawing.Size(49, 36);
+            this.dfButton.TabIndex = 4;
+            this.dfButton.Text = "ЦФ";
+            this.dfButton.UseVisualStyleBackColor = true;
+            this.dfButton.Click += new System.EventHandler(this.dfButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(224, 511);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 69);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.TabStop = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(375, 511);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(145, 69);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.TabStop = false;
+            // 
+            // irPictureBox
+            // 
+            this.irPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.irPictureBox.BackColor = System.Drawing.SystemColors.Window;
+            this.irPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.irPictureBox.Location = new System.Drawing.Point(2, 188);
+            this.irPictureBox.Name = "irPictureBox";
+            this.irPictureBox.Size = new System.Drawing.Size(829, 150);
+            this.irPictureBox.TabIndex = 9;
+            this.irPictureBox.TabStop = false;
+            // 
+            // signalPictureBox
+            // 
+            this.signalPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.signalPictureBox.BackColor = System.Drawing.SystemColors.Window;
+            this.signalPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.signalPictureBox.Location = new System.Drawing.Point(2, 19);
+            this.signalPictureBox.Name = "signalPictureBox";
+            this.signalPictureBox.Size = new System.Drawing.Size(829, 150);
+            this.signalPictureBox.TabIndex = 10;
+            this.signalPictureBox.TabStop = false;
+            this.signalPictureBox.VisibleChanged += new System.EventHandler(this.signalPictureBox_VisibleChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходный сигнал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Отсчеты импульсной характеристики h(i). Порядок фильтра M = 65";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Сигнал после ЦФ";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(677, 544);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(154, 36);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "Выход";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "dat";
+            this.openFileDialog1.Filter = "DAT Files (*.dat)|*.dat|Все файлы (*.*)|*.*";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(526, 511);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(145, 69);
+            this.listBox3.TabIndex = 11;
+            this.listBox3.TabStop = false;
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Location = new System.Drawing.Point(2, 511);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(216, 26);
+            this.readFileButton.TabIndex = 1;
+            this.readFileButton.Text = "Чтение файла";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            this.readFileButton.Click += new System.EventHandler(this.readFileButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(680, 524);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(711, 524);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(742, 524);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(773, 524);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(804, 524);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 16;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(677, 508);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Выберите канал";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 583);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.readFileButton);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.signalPictureBox);
+            this.Controls.Add(this.irPictureBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dfButton);
+            this.Controls.Add(this.irButton);
+            this.Controls.Add(this.signalButton);
+            this.Controls.Add(this.dfPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Цифровая фильтрация методом ДПФ";
+            ((System.ComponentModel.ISupportInitialize)(this.dfPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.irPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signalPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dfPictureBox;
+        private System.Windows.Forms.Button signalButton;
+        private System.Windows.Forms.Button irButton;
+        private System.Windows.Forms.Button dfButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox irPictureBox;
+        private System.Windows.Forms.PictureBox signalPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button readFileButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FIRfilter
+{
+    public partial class Form1 : Form
+    {
+        public float[] xk = new float[500];
+        public float[] yk = new float[500];
+        float[]  h= new float[500];
+        int M = 56;
+        int N0 = 500;
+        float w1=20;
+        float w2=60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(int k, int N, PointF pt0, PointF ptmin, PointF ptmax, float[] xs, PictureBox pb)
+        {
+            Graphics gr = pb.CreateGraphics();
+
+            float h = pb.Height;
+            float w = pb.Width;
+            float x, y, y1, y2, x1, x2;
+            float K;
+
+            PointF scaleXY;
+            //scaleXY = new PointF(ptmax.X / 10, ptmax.Y * 2);
+            scaleXY = new PointF(((w - 2 * pt0.X) / (ptmax.X - ptmin.X)), ((h) / (ptmax.Y - ptmin.Y)));
+
+            DrawAxes(k, N, pt0, scaleXY, ptmax, pb);
+
+            Pen p = new Pen(Color.Red);
+            //  scaleXY= new PointF (((w - 2*pt0.X) / (ptmax.X - ptmin.X)),((h) / (ptmax.Y - ptmin.Y)));
+
+            if (k == 1) K = ptmax.X - 1;
+            else K = ptmax.X;
+
+            for (int i = 0; i < K; i++)
+            {
+                if (k == 1) { y1 = xs[i]; y2 = xs[i + 1]; x1 = i; x2 = i + 1; }
+                else { y1 = 0; y2 = xs[i]; x1 = i; x2 = i; }
+                x = x1 * scaleXY.X + pt0.X;
+                y = pt0.Y - y2 * scaleXY.Y;
+                gr.DrawLine(p, x1 * scaleXY.X + pt0.X, pt0.Y - (y1 * scaleXY.Y), pt0.X + x2 * scaleXY.X, pt0.Y - y2 * scaleXY.Y);
+
+            }
+            gr.Dispose();
+        }
+
+        public void DrawAxes(int k, int N, PointF pt0, PointF scaleXY, PointF ptmax, PictureBox pb)
+        {
+            Graphics gr = pb.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+            //p.Color = Color.Black;
+            float h = pb.Height;
+            float w = pb.Width;
+            float dx = scaleXY.X;
+            float dy = scaleXY.Y;
+            float x = 0;
+            float y = 0;
+            float y1, y2;
+
+            //tmp = new float[(int)scaleXY.X];
+            gr.DrawLine(p, pt0.X, 0, pt0.X, h);
+            gr.DrawLine(p, pt0.X, pt0.Y, w - pt0.X, pt0.Y);
+            if ((k == 1) && (N == 5)) { y1 = 0; y2 = h; }
+            else { y1 = pt0.Y + 6; y2 = pt0.Y - 6; }
+            //p.Width = 2F;
+            for (int i = 0; i <= N; i++)
+            {
+
+                x = dx * ptmax.X * i / N;
+                // tmp[i] = x;
+                gr.DrawLine(p, x + pt0.X, y1, x + pt0.X, y2);
+
+            }
+            for (int i = 0; i < ptmax.Y * 2; i++)
+            {
+                gr.DrawLine(p, pt0.X, y, pt0.X + 10, y);
+                y += dy;
+            }
+            gr.Dispose();
+        }
+
+        public void ImpResponse(float[] xs, float[] h, float M, float N0, float w1, float w2)
+        {
+            double hk;
+            for (int k = 0; k < M; k++)
+			{
+
+
+                if (k != (M / 2 + 1)) { hk = (1 / (Math.PI * (k - M / 2 - 1))) * (Math.Sin(w2 / (2*N0) * Math.PI * (k - M / 2 - 1)) - Math.Sin(w1 / (2*N0) * Math.PI * (k - M / 2 - 1))); h[k] = (float)hk; }
+                else h[k] = 40f / (2*N0);
+
+        
+            }
+        }
+
+        public void FIR(float[] yk, float[] h, int M, int N0)
+        {
+            for (int i = M/2; i<=(N0-M/2-1) ; i++)
+            {
+                yk[i] = 0;
+                for (int j = 0; j <= M; j++)
+                {
+                    yk[i] += xk[i - M / 2 + j] * h[j];
+                }
+            }
+        }        
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+      
+
+        private void signalButton_Click(object sender, EventArgs e)
+        {
+            
+            float h = signalPictureBox.Height;
+            
+            PointF pt0 = new PointF(10, h / 2);
+            PointF ptmax = new PointF(500, ((float)Math.Round(xk.Max())));
+            if (ptmax.Y < xk.Max()) ptmax.Y++;
+            PointF ptmin = new PointF(0, -ptmax.Y);
+            
+            signalPictureBox.Refresh();
+            Draw(1, 10, pt0, ptmin, ptmax, xk, signalPictureBox);
+        }
+
+        private void irButton_Click(object sender, EventArgs e)
+        {
+            //float w1 = 140;
+            //float w2 = 180;
+            ImpResponse(xk, h, M, N0, w1, w2);
+            for (int i = 0; i < M; i++)
+            {
+                listBox2.Items.Add(Convert.ToString(i) + "       " + Convert.ToString(h[i]));
+            }
+            PointF pt0 = new PointF(10, irPictureBox.Height/2);
+            PointF ptmin = new PointF(0, -0.05f);
+            PointF ptmax = new PointF(M, 0.05f);
+            irPictureBox.Refresh();
+            Draw(0, 1, pt0, ptmin, ptmax, h, irPictureBox);
+
+        }
+
+        private void dfButton_Click(object sender, EventArgs e)
+        {
+            FIR(yk, h, M, N0);
+            PointF pt0 = new PointF(10, dfPictureBox.Height/2);
+            PointF ptmin = new PointF(0, -2);
+            PointF ptmax = new PointF(500, 2);
+            dfPictureBox.Refresh();
+            Draw(1, 5,pt0, ptmin, ptmax, yk, dfPictureBox);
+            for (int i = M/2; i<=(N0-M/2-1) ; i++)
+            {
+                listBox3.Items.Add(Convert.ToString(i) + "       " + Convert.ToString(yk[i]));
+            }
+        }
+
+        private void signalPictureBox_VisibleChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void readFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (BinaryReader b = new BinaryReader(File.Open(openFileDialog1.FileName, FileMode.Open)))
+                {
+                    this.Text = "Цифровая фильтрация методом ДПФ";
+                    int pos = 0;
+                    int i = 0;
+                    float length = b.BaseStream.Length;
+                    while (pos < length)
+                    {
+
+                        xk[i] = b.ReadSingle();
+                        listBox1.Items.Add(Convert.ToString(i + 1) + "       " + Convert.ToString(xk[i]));
+                        i++;
+                        pos += sizeof(float);
+
+                    }
+                    b.Close();
+                    signalButton.Enabled = true;
+                    irButton.Enabled = true;
+                    dfButton.Enabled = true;
+
+                    int k = openFileDialog1.FileName.LastIndexOf('\\');
+                    this.Text += "              " + openFileDialog1.FileName.Substring(k + 1);
+                    b.Dispose();
+                    openFileDialog1.Reset();
+                }
+            }
+        }
+
+        
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            w1 = 20;
+            w2 = 60;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            w1 = 60;
+            w2 = 100;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            w1 = 100;
+            w2 = 140;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            w1 = 140;
+            w2 = 180;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            w1 = 180;
+            w2 = 220;
+        }
+
+        }
+    }
+
